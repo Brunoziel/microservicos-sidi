49f99b111ab3a27f6790dcdff21504bc472c6474
Italo Monteiro <italovitordasilvamonteiro@gmail.com>
2023-05-10 21:29:34 -0300

    criando pagina de login

--- front_end/scripts/script.js
+++ front_end/scripts/script.js
@@ -3,7 +3,7 @@ function sendMessage() {
   document.getElementById("chatbox").innerHTML += "<p><strong>Você:</strong> " + userMessage + "</p>";
   // Aqui você pode usar uma API para processar a mensagem do usuário e gerar uma resposta automática.
   // Neste exemplo, estou simplesmente respondendo com uma mensagem fixa.
-  var botMessage = "Olá! Eu sou um chatbot. Como posso ajudar?";
+  var botMessage = "Okay, você escolheu a vaga: " + document.getElementById("usermsg").value;
   document.getElementById("chatbox").innerHTML += "<p><strong>Bot:</strong> " + botMessage + "</p>";
   document.getElementById("usermsg").value = "";
   return false;
