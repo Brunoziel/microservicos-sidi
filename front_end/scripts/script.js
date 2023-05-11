f5e854436e49843e8680082f8b0223a1d6787715
Italo Monteiro <italovitordasilvamonteiro@gmail.com>
2023-05-10 21:09:35 -0300

    estilizando o chatbot

--- front_end/scripts/script.js
+++ front_end/scripts/script.js
@@ -1,10 +1,16 @@
-function sendMessage() {
+function sendMessage() {  
   var userMessage = document.getElementById("usermsg").value;
-  document.getElementById("chatbox").innerHTML += "<p><strong>You:</strong> " + userMessage + "</p>";
+  document.getElementById("chatbox").innerHTML += "<p><strong>Você:</strong> " + userMessage + "</p>";
   // Aqui você pode usar uma API para processar a mensagem do usuário e gerar uma resposta automática.
   // Neste exemplo, estou simplesmente respondendo com uma mensagem fixa.
   var botMessage = "Olá! Eu sou um chatbot. Como posso ajudar?";
   document.getElementById("chatbox").innerHTML += "<p><strong>Bot:</strong> " + botMessage + "</p>";
   document.getElementById("usermsg").value = "";
   return false;
+}
+
+function removeMessage(){    
+  const paragrafo = document.getElementById("bemVindo");
+  // Remove o parágrafo
+  paragrafo.remove();
 }
